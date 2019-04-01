c81b759ba3649cf4a3985eddbe3bc9eca91e73c6
David Adewoyin <david.adewoyin@fullstackacademy.com>
2019-04-01 00:24:25 -0400

    startingggG

---
+++ data.js
@@ -0,0 +1,16 @@
+export const data = {
+  posts: [
+    {
+      id: 1,
+      text: "Hey check me outttt",
+    },
+    {
+      id: 2,
+      text: "I love this song",
+    },
+    {
+      id: 3,
+      text: "Heyyyyy another post",
+    }
+  ]
+}
---
+++ src/client/config/config.js
@@ -0,0 +1,6 @@
+export default {
+  apiKey: "AIzaSyClpKDyBBTrMMItJN-v8UwnJHQzbiPepdA",
+  authDomain: "my-awesome-project1-3fbe0.firebaseapp.com",
+  databaseURL: "https://my-awesome-project1-3fbe0.firebaseio.com",
+  storageBucket: "my-awesome-project1-3fbe0.appspot.com",
+}
